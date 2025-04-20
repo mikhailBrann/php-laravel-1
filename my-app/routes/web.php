96fb8b8ff75f3c5308bf12b9b1253ac14faded9b
Mikhail Brann <alunos13@yandex.com>
2025-04-20 18:12:22 +0300

    middleware: added checkAuth MW

--- my-app/routes/web.php
+++ my-app/routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/is_auth', [App\Http\Controllers\HomeController::class, 'isAuth'])
+    ->name('isAuth')
+    ->middleware('check-auth');
